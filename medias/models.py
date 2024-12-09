ede985694d5a6ac0207f82178b65926cd89bfb35
Leeminsu67 <alstn67@gmail.com>
2024-12-10 00:20:02 +0900

    medias 앱 생성(사진과 동영상 저장)

---
+++ medias/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from common.models import CommonModel
+
+
+class Photo(CommonModel):
+
+    file = models.ImageField()
+    description = models.CharField(
+        max_length=140,
+    )
+    room = models.ForeignKey(
+        "rooms.Room",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+    )
+    experience = models.ForeignKey(
+        "experiences.Experience",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return "Photo File"
+
+
+class Video(CommonModel):
+
+    file = models.FileField()
+    # OneToOneField를 사용하면 이 활동에 종속되고 다른 어떠한 동영상도 이 활동에 종속될 수 없다
+    # 활동은 하나의 영상만 가질 수 있다.
+    experience = models.OneToOneField(
+        "experiences.Experience",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return "Video File"
