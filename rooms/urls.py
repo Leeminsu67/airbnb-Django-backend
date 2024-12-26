ae8dcd5d6c2a648af60f703570719daf16064361
Leeminsu67 <alstn67@gmail.com>
2024-12-27 00:52:33 +0900

    Amenity 모델 관련 API 생성

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.see_all_rooms),
-    # 꺽쇠를 열어주고 parameter의 타입을 적어준 다음에 꺽쇠를 닫으면 된다
-    # 장고가 url에 있는 타입을 확인해준다.
-    path("<int:room_pk>/", views.see_one_room),
+    path("amenities/", views.Amenities.as_view()),
+    path("amenities/<int:pk>/", views.AmenityDetail.as_view()),
 ]
