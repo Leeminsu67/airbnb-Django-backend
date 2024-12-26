eac9ae9be5d467b5e27c1cdcd0cebcb6fb015b5b
Leeminsu67 <alstn67@gmail.com>
2024-12-26 23:38:12 +0900

    함수형 API 클래스형 APIView로 변경

--- categories/urls.py
+++ categories/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.categories),
-    path("<int:pk>/", views.category),
+    # as_view가 하는 일은 해당 클래스에 GET 메서드이면 get함수 실행 POST이면 post함수 실행하는 역할
+    path("", views.Categories.as_view()),
+    path("<int:pk>/", views.CategoryDetail.as_view()),
 ]
--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,4 @@
+from rest_framework.views import APIView
 from .models import Category
 from rest_framework.exceptions import NotFound
 from rest_framework.decorators import api_view
@@ -6,14 +7,14 @@ from rest_framework.status import HTTP_204_NO_CONTENT
 from .serializers import CategorySerializer
 
 
-@api_view(["GET", "POST"])
-def categories(request):
+class Categories(APIView):
 
-    if request.method == "GET":
+    def get(self, request):
         all_categories = Category.objects.all()
         serializer = CategorySerializer(all_categories, many=True)
         return Response(serializer.data)
-    elif request.method == "POST":
+
+    def post(self, request):
         serializer = CategorySerializer(data=request.data)
 
         if serializer.is_valid():
@@ -26,24 +27,22 @@ def categories(request):
             return Response(serializer.errors)
 
 
-# {"name": "Category from DRF", "kind": "rooms"}
-# {"kind": "lalalalal"}
+class CategoryDetail(APIView):
 
+    def get_object(self, pk):
+        try:
+            return Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            # 에러 반환
+            raise NotFound
 
-@api_view(["GET", "PUT", "DELETE"])
-def category(request, pk):
-    try:
-        category = Category.objects.get(pk=pk)
-    except Category.DoesNotExist:
-        # 에러 반환
-        raise NotFound
-
-    if request.method == "GET":
-        serializer = CategorySerializer(category)
+    def get(self, request, pk):
+        serializer = CategorySerializer(self.get_object(pk))
         return Response(serializer.data)
-    elif request.method == "PUT":
+
+    def put(self, request, pk):
         serializer = CategorySerializer(
-            category,
+            self.get_object(pk),
             data=request.data,
             # 여기로 들어오는 데이터가 완벽한 형태가 아닐수도 있다는걸 알게됨
             partial=True,
@@ -56,6 +55,7 @@ def category(request, pk):
             )
         else:
             return Response(serializer.errors)
-    elif request.method == "DELETE":
-        category.delete()
+
+    def delete(self, request, pk):
+        self.get_object(pk).delete()
         return Response(status=HTTP_204_NO_CONTENT)
